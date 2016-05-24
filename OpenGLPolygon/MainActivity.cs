a9bfb37e4bfaf140a45a6b8d059fd89f483448f0
gesneriana <694060865@qq.com>
2016-05-24 22:32:38 +0800

    C#开发Android的OpenGL多边形绘图的测试代码

--- OpenGLPolygon/MainActivity.cs
+++ OpenGLPolygon/MainActivity.cs
@@ -5,26 +5,29 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Opengl;
+using Android.Util;
 
 namespace OpenGLPolygon
 {
     [Activity(Label = "OpenGLPolygon", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
+            // 创建一个GLSurfaceView,用于显示OpenGL绘制的图形
+            GLSurfaceView glView = new GLSurfaceView(this);
 
             // Get our button from the layout resource,
             // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
-
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            // 创建GLSurfaceView的内容绘制器
+            MyRenderer myRender = new MyRenderer();
+            glView.SetRenderer(myRender);
+            SetContentView(glView);
+            Log.Debug("调试信息", "handle: " + this.Handle);
         }
     }
 }
